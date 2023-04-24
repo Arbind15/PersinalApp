8bdf34558c9c0af9fcb9824a4f67b42f0d15e003
Arbind Mehta <arbindmehta@Arbinds-MacBook-Pro.local>
2023-04-24 16:08:00 +0545

    initial commit

--- personal_app/settings.py
+++ personal_app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os, django_heroku
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,5 +127,3 @@ else:
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
