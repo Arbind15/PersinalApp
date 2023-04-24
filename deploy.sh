e459e4bb14c092ac1e530d70158b5f40a88416c0
Arbind Mehta <arbindmehta@Arbinds-MacBook-Pro.local>
2023-04-24 21:55:02 +0545

    initial commit2

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting deployment..."
-source /home2/mfgywmcc/virtualenvs/venv/bin/activate
-cd /home2/mfgywmcc/public_html/personal_app
+source /home2/mfgywmcc/virtualenv/MainApp/3.8/bin/activate
+cd /home2/mfgywmcc/public_html
 git pull origin master
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
